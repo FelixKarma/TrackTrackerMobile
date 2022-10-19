0a94f6d53acc875ebb59a0f2a8bbc93b32ee6120
famine hokusai <lbclbc59000@gmail.com>
2022-10-19 16:26:48 +0200

    [jb][footer]

---
+++ assets/tracktracker.js
@@ -0,0 +1,9 @@
+const got = {
+    titre-serie : "Game of thrones",
+    image : "got.png",
+    realisteur : "David Benioff",
+    date_sortie : "17 Avril 2011",
+    nb_saisons : 8,
+    genre : ["Fantasy", "Drama" "Gore"],
+    actors : []
+}
