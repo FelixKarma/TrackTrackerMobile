49a9bc88e413e2059b2bf06b5df97a06fbe39f95
99788879 <gre.fontaine@gmail.com>
2022-10-19 16:33:30 +0200

    [FG][FEAT]cgt bouttons

---
+++ assets/tracktracker.js
@@ -0,0 +1,25 @@
+const avatar ={
+    titre_film:"Avatar",
+    image : "avatarlarge.jpg",
+    realisateur:"James Cameron",
+    date_sortie:"16 Décembre 2009",
+    duree:162,
+    genre:["Science-Fiction", "Action", "Aventure", "Fantasy"],
+    actors : ["Sam Worthington","Zoe Saldana","Sigourney Weaver","Stephen Lang"],
+    synopsis: "Jake Sully, ancien marine aujourd'hui paraplégique, est envoyé en mission sur la lointaine planète Pandora, pour remplacer son défunt frère jumeau. Il intègre le programme Avatar, dirigé par Grace Augustine, afin de mener des contacts diplomatiques avec les Na'vis, l'espèce habitant la planète. En effet, leur village se trouve sur un gisement d'Unobtanium, un minerai qui explique la présence humaine sur l'exo-lune. Mais en se rapprochant d'eux, et surtout de Neytiri, Jake va voir le combat sous un autre angle et va finir par se retourner contre son propre camp...",
+    musique:[musique1avatar]
+}
+
+const musique1avatar ={
+    titre :" I See You (Theme from Avatar)",
+    compositeur:"Léona Lewis",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores repellendus ab vel enim. Laborum, dolore. Nulla sed dolorum fugiat explicabo asperiores, adipisci modi necessitatibus fugit!",
+    track:"iSeeYou.mp3"
+}
+
+
+// //James Cameron
+
+// 16/12/2009
+
+// Science fiction//
