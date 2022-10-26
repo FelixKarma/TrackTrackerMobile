90ecfcd974cfeb63d79970398a1b0622dcd57ec7
MarikaElmak <elmakrini.ka@gmail.com>
2022-10-26 09:19:55 +0200

    [ek][feat}

---
+++ assets/tracktracker.js
@@ -0,0 +1,17 @@
+const avatar = {
+    titre-serie: "retours vers le futur",
+    image: "retour vers le futur",
+    realisateur: "Robert Zemeckis",
+    date de sortie: "16 decembre 2009"
+    duré du film: 118,
+    genre : ["science fiction"],
+    actor : [Michael J. Fox,Christopher Lloyd,Thomas F. Wilson,Lea Thompson]
+    synopsis:Le 25 octobre 1985, Marty McFly, adolescent ordinaire âgé de 17 ans, vivant à Hill Valley (ville fictive de Californie probablement située dans le Marin County près de San Francisco), découvre l’existence d’une machine à voyager dans le temps, construite par son ami le docteur Emmett Brown, sous la forme d’une voiture (une DeLorean). À la suite d'une fusillade, Marty, prenant la fuite à bord de la DeLorean, se retrouve accidentellement projeté en novembre 1955. Il cherchera un moyen de regagner 1985. Mais sa présence dans une époque où il n’est pas encore né a déjà menacé sa propre existence…
+musique:Alan Silvestri
+}
+const musique1RVLF = {
+    titre: "...",
+    compositeur:"...",
+    descreption: "......",
+    tract : "......"
+}
